7c350853b2ad65a51c330f7e8c4533113146abab
tera <thecodss@gmail.com>
2021-07-11 22:46:49 +0900

    styled custom-button component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import { createStructuredSelector } from "reselect";
 
@@ -13,7 +12,7 @@ import { selectCurrentUser } from "../../redux/user/user.selector";
 
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink  } from "./header.styles";
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink  } from "./header.styles";
 
 const Header = ({ currentUser, hidden }) => (
   <HeaderContainer>
@@ -28,10 +27,10 @@ const Header = ({ currentUser, hidden }) => (
         CONTACT
       </OptionLink>
       {currentUser ? (
-        <OptionDiv onClick={() => auth.signOut()}>
+        <OptionLink as='div' onClick={() => auth.signOut()}>
           {" "}
           SIGN OUT
-        </OptionDiv>
+        </OptionLink>
       ) : (
         <OptionLink to="/signin">
           SIGN IN
