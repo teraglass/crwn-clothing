97d059826755ddad3af5dc0d83f81db05db263d6
teraglass <thecodss@gmail.com>
2021-07-13 09:37:19 +0900

    implement code that pulls in shop data from firestore and stores it into shop reducer from shop page. Also included WithSpinner HOC

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -24,12 +24,12 @@ class ShopPage extends React.Component {
     const {updateCollections} = this.props;
     const collectionRef = firestore.collection('collections');
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-      const collectionsMap =convertCollectionsSnapshotToMap(snapshot);
+
+    collectionRef.get.then(snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({loading : false});
-
-    })
+    });
   }
   render() {
     const { match } = this.props;
--- src/redux/store.js
+++ src/redux/store.js
@@ -14,3 +14,5 @@ export const store = createStore(rootReducer, applyMiddleware(...middelewares));
 
 export const persistor = persistStore(store);
 
+
+
