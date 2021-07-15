653da82198d311f3abe8cfca60527da2d6cd6d7e
tera <thecodss@gmail.com>
2021-07-15 21:30:46 +0900

    235

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios'
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Succesful!');
+   axios({
+     url: 'payment',
+     method:'post',
+     data: {
+       amount: priceForStripe,
+       token: token
+     }
+   }).then(response => {
+     alert('Payment successful')
+   }).catch(error => {
+     console.log('Payment error: ', error);
+     alert(
+       'There was an issue with your payment. Please sure you use provided credit card.'
+     )
+   })
   };
 
   return (
